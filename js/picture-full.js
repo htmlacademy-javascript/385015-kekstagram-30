fe8a08ab49dc5f14688c6b38d6a547e499f551c5
duhtpa <dor_alex@inbox.ru>
2023-10-29 20:48:02 +0700

    ДЗ-8. Обработка открытия и закрытия изображения

---
+++ js/picture-full.js
@@ -0,0 +1,74 @@
+const COMMENT_SHOW_COUNT = 5; // пусть будет - потом пригодится
+const modal = document.querySelector('.big-picture');
+const imageURL = modal.querySelector('.big-picture__img img');
+const imageDescription = modal.querySelector('.social__caption');
+const imageLikesCount = modal.querySelector('.likes-count');
+const imageCommentsCount = modal.querySelector('.social__comment-count');
+const imageCommentsCountTotal = imageCommentsCount.querySelector(
+  '.social__comment-total-count'
+);
+const imageCommentsCountShown = imageCommentsCount.querySelector(
+  '.social__comment-shown-count'
+);
+const commentsContainer = modal.querySelector('.social__comments');
+const commentsLoader = modal.querySelector('.comments-loader');
+const buttonClose = modal.querySelector('.big-picture__cancel');
+
+const templateFragment = document.querySelector('#comment').content;
+const template = templateFragment.querySelector('.social__comment');
+
+const fragment = document.createDocumentFragment();
+
+const getComments = (comments) => {
+  comments.forEach((commentData) => {
+    const commentTemplate = template.cloneNode(true);
+    commentTemplate.querySelector('.social__picture').src = commentData.avatar;
+    commentTemplate.querySelector('.social__text').textContent =
+      commentData.message;
+    fragment.append(commentTemplate);
+  });
+};
+
+const onDocumentKeydown = (evt) => {
+  if (evt.key === 'Escape' || evt.key === 'Enter') {
+    evt.preventDefault();
+    closeModal();
+  }
+};
+
+const onButtonCloseClick = () => {
+  closeModal();
+};
+
+function closeModal() {
+  document.body.classList.remove('modal-open');
+  modal.classList.add('hidden');
+
+  commentsContainer.innerHTML = '';
+
+  document.removeEventListener('keydown', onDocumentKeydown);
+  buttonClose.removeEventListener('click', onButtonCloseClick);
+}
+
+function openModal({ url, likes, description, comments }) {
+  imageURL.src = url;
+  imageDescription.textContent = description;
+  imageLikesCount.textContent = likes;
+  imageCommentsCountTotal.textContent = comments.length;
+  imageCommentsCountShown.textContent =
+    comments.length < COMMENT_SHOW_COUNT ? comments.length : COMMENT_SHOW_COUNT;
+
+  getComments(comments);
+
+  commentsContainer.append(fragment);
+
+  imageCommentsCount.classList.add('hidden');
+  commentsLoader.classList.add('hidden');
+  document.body.classList.add('modal-open');
+  modal.classList.remove('hidden');
+
+  document.addEventListener('keydown', onDocumentKeydown);
+  buttonClose.addEventListener('click', onButtonCloseClick);
+}
+
+export { openModal };
--- js/render.js
+++ js/render.js
@@ -1,18 +1,52 @@
 const container = document.querySelector('.pictures');
+import { openModal } from './picture-full.js';
 
 const templateFragment = document.querySelector('#picture').content;
 const template = templateFragment.querySelector('.picture');
 
 const fragment = document.createDocumentFragment();
 
-const fillDataTemplate = (element, { url, description, likes, comments }) => {
+const fillDataTemplate = (
+  element,
+  { id, url, description, likes, comments }
+) => {
   element.querySelector('.picture__img').src = url;
   element.querySelector('.picture__img').alt = description;
   element.querySelector('.picture__comments').textContent = comments.length;
   element.querySelector('.picture__likes').textContent = likes;
+
+  // получение массива комментариев происходит через id изображения
+  // id может быть у превью/фулл изображения и аватарки
+  // поэтому чтобы id не смогли "пересекаться" - я сделал через добавление скрытого узла
+  const picureID = document.createElement('input');
+  picureID.classList.add('picture__id');
+  picureID.type = 'hidden';
+  picureID.textContent = id;
+
+  element.append(picureID);
+};
+
+const clickPreview = (dataArray) => {
+  container.addEventListener('click', (evt) => {
+    if (evt.target.closest('.picture')) {
+      evt.preventDefault();
+
+      const preview = evt.target.closest('.picture');
+      const pictureID = Number(
+        preview.querySelector('.picture__id').textContent
+      );
+      const pictureObject = dataArray.filter(
+        (item) => item.id === pictureID
+      )[0];
+
+      openModal(pictureObject);
+    }
+  });
 };
 
 const render = (dataArray) => {
+  document.createElement('ul');
+
   dataArray.forEach((item) => {
     const element = template.cloneNode(true);
 
@@ -22,6 +56,7 @@ const render = (dataArray) => {
   });
 
   container.append(fragment);
+  clickPreview(dataArray);
 };
 
 export { render };
